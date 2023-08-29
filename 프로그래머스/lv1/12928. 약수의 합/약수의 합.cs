79dbb8310dc2ebc2802964b9b96fa6c9264a10f4
이경민(Kyung Min Lee) <114645806+kminsmin@users.noreply.github.com>
2023-08-29 09:05:14 +0900

    [level 1] Title: 약수의 합, Time: 0.19 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (n%i==0 && n > 0)
+            {
+                sum += i;
+            }
+        }
+        int answer = sum;
+        return answer;
+    }
+}
