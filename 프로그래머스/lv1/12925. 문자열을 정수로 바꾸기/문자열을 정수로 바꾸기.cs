f2338e87be9960ba4411bbb378decfe4af966882
이경민(Kyung Min Lee) <114645806+kminsmin@users.noreply.github.com>
2023-08-30 10:01:28 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.78 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cs
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cs
@@ -1,25 +1,6 @@
 public class Solution {
     public int solution(string s) {
-        int answer = 0;
-        char[] arr = s.ToCharArray();
-        if (char.IsDigit(arr[0]))
-        {
-            answer = int.Parse(s);
-        }
-        else
-        {
-            if (arr[0].ToString() == "+")
-            {
-                s = s.Substring(1, s.Length-1);
-                answer = int.Parse(s);
-            }
-            else
-            {
-                s = s.Substring(1, s.Length-1);
-                answer = -int.Parse(s);
-            }
-            
-        }
+        int answer = int.Parse(s);
         return answer;
     }
 }
