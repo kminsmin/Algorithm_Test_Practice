b2326b2a0dcca6da54afba0461d839d850b90c96
이경민(Kyung Min Lee) <114645806+kminsmin@users.noreply.github.com>
2023-09-04 10:04:49 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.29 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public string solution(int n) {
+        string answer = "";
+        for (int i = 1; i <= n; i++)
+        {
+            if (i%2 != 0)
+            {
+                answer += "수";
+            }
+            else
+            {
+                answer += "박";
+            }
+        }
+        return answer;
+    }
+}
