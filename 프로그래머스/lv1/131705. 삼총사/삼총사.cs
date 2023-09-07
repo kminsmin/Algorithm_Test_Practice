d89f8eb62515b47c7c5db2bf082350836ed6732e
이경민(Kyung Min Lee) <114645806+kminsmin@users.noreply.github.com>
2023-09-07 10:42:43 +0900

    [level 1] Title: 삼총사, Time: 0.21 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+
+        int checkTrio = 0;
+
+        int numLength = number.Length;
+        
+        for(int i = 0; i <= numLength - 3; i++)
+        {
+            for(int j = i+1; j <= numLength - 2; j++)
+            {
+                for(int k = j+1; k <= numLength -1 ; k++)
+                {
+                    checkTrio += (number[i] + number[j] + number [k]);
+                    answer += (checkTrio == 0) ? 1 : 0;
+                    checkTrio = 0;
+                }
+                
+            }
+        }
+        
+
+        return answer;
+    }
+}
