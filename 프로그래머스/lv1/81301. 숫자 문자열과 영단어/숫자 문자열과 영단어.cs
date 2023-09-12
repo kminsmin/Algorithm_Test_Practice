3a91318f7ead6b05ee917414935049cafaaab5f1
이경민(Kyung Min Lee) <114645806+kminsmin@users.noreply.github.com>
2023-09-12 10:13:32 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.75 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
@@ -2,67 +2,17 @@ using System;
 
 public class Solution {
     public int solution(string s) {
-        string answer = "";
-        string tempString = "";
-        char[] temp = s.ToCharArray();
-        for(int i = 0; i < temp.Length; i++)
-        {
-            if(char.IsDigit(temp[i]))
-             {
-                 answer += temp[i].ToString();
-                 continue;
-             }
-            else
-               {
-                   tempString += temp[i].ToString();
-                   switch(tempString)
-                   {
-                       case "zero":
-                           answer += "0";
-                           tempString = "";
-                           break;
-                       case "one":
-                           answer += "1";
-                           tempString = "";
-                           break;
-                       case "two":
-                           answer += "2";
-                           tempString = "";
-                           break;
-                       case "three":
-                           answer += "3";
-                           tempString = "";
-                           break;
-                       case "four":
-                           answer += "4";
-                           tempString = "";
-                           break;
-                       case "five":
-                           answer += "5";
-                           tempString = "";
-                           break;
-                       case "six":
-                           answer += "6";
-                           tempString = "";
-                           break;
-                       case "seven":
-                           answer += "7";
-                           tempString = "";
-                           break;
-                       case "eight":
-                           answer += "8";
-                           tempString = "";
-                           break;
-                       case "nine":
-                           answer += "9";
-                           tempString = "";
-                           break;
-                       default:
-                           break;
-                   }
-               }
-        }
-        return int.Parse(answer);
+        s = s.Replace("zero", "0");
+        s = s.Replace("one", "1");
+        s = s.Replace("two", "2");
+        s = s.Replace("three", "3");
+        s = s.Replace("four", "4");
+        s = s.Replace("five", "5");
+        s = s.Replace("six", "6");
+        s = s.Replace("seven", "7");
+        s = s.Replace("eight", "8");
+        s = s.Replace("nine", "9");
+        return int.Parse(s);
     }
                
 }
