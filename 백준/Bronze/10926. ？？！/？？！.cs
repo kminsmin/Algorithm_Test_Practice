24fa8a33594fd037ba6f80f8e4a94a986fe2db44
이경민(Kyung Min Lee) <114645806+kminsmin@users.noreply.github.com>
2024-02-09 16:19:48 +0900

    [Bronze V] Title: ??!, Time: 92 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Baekjoon{
+    class Program{
+        static void Main(){
+            string input = Console.ReadLine();
+            Console.WriteLine(input + "??!");
+        }
+    }
+}
