3c8f0f478a4a0a090c437ab32bc6fc036eccb8a8
이경민(Kyung Min Lee) <114645806+kminsmin@users.noreply.github.com>
2024-02-09 17:43:23 +0900

    [Bronze V] Title: 개, Time: 56 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Baekjoon{
+    class Program{
+        static void Main(){
+            string dog = "|" + @"\" + "_/|\n" + "|q p|   /}\n" + "( 0 )\"\"\"" + @"\" + "\n"
+                + "|\"^\"`    |\n" + "||_/=" + @"\" + @"\" + "__|";
+            Console.WriteLine(dog);
+        }
+    }
+}
