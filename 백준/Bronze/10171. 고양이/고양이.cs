f75e11e56b7d2c6abcb602983c5d23256e7ca679
이경민(Kyung Min Lee) <114645806+kminsmin@users.noreply.github.com>
2024-02-09 17:38:44 +0900

    [Bronze V] Title: 고양이, Time: 56 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Baekjoon{
+    class Program{
+        static void Main(){
+            string cat = @"\    /"+ @"\" + "\n )  ( ')" + "\n(  /  )" + "\n " + @"\"
+                        + "(__)|";
+            Console.WriteLine(cat);
+        }
+    }
+}
