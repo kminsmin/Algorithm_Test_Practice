f18a52727f93d871403f6c71ff19f14d4f262152
이경민(Kyung Min Lee) <114645806+kminsmin@users.noreply.github.com>
2024-02-21 20:19:08 +0900

    [Bronze V] Title: 아스키 코드, Time: 64 ms, Memory: 5448 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Baekjoon{
+    class Program{
+        static void Main(){
+            char ch = char.Parse(Console.ReadLine());
+            Console.WriteLine((int)ch);
+        }
+    }
+}
