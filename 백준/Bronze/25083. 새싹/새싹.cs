e24b7b93b58650d27d8b7dc93f60547750cb7f2d
이경민(Kyung Min Lee) <114645806+kminsmin@users.noreply.github.com>
2024-03-05 18:27:13 +0900

    [Bronze V] Title: 새싹, Time: 60 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Baekjoon{
+    class Program{
+        static void Main(){
+            string sprout = "";
+            sprout += "         ,r\'\"7\n";
+            sprout += "r`-_   ,\'  ,/ \n";
+            sprout += " \\. \". L_r\'\n";
+            sprout += "   `~\\/\n";
+            sprout += "      |\n";
+            sprout += "      |";
+            Console.WriteLine(sprout);
+        }
+    }
+}
