6c25101c906a2d00f64d13453b4197e2319c5859
이경민(Kyung Min Lee) <114645806+kminsmin@users.noreply.github.com>
2024-05-28 11:20:53 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 64 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Baekjoon{
+    class Program{
+        static void Main(){
+            int n = 0;
+            
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n);
+            Console.WriteLine(1);
+        }
+    }
+}
