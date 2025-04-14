71d7f75dd19c80ce255a13e7777aa0ec3a0dff31
이경민(Kyung Min Lee) <114645806+kminsmin@users.noreply.github.com>
2025-04-14 13:38:17 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2976 ms, Memory: 22732 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+
+namespace Baekjoon{
+    class Program{
+        static void Main(){
+            int count = int.Parse(Console.ReadLine());
+            int[,] coords = new int[count,2];
+            for(int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                string[] digits = input.Split(' ');
+                coords[i,0] = int.Parse(digits[0]);
+                coords[i,1] = int.Parse(digits[1]);
+            }
+            var sorted = Enumerable.Range(0,coords.GetLength(0))
+                .Select(i => new
+                        {
+                            First = coords[i,0],
+                            Second = coords[i,1]
+                        })
+                .OrderBy(x => x.Second)
+                .ThenBy(x => x.First);
+            foreach(var element in sorted)
+            {
+                Console.WriteLine($"{element.First} {element.Second}");
+            }
+        }
+    }
+}
