6fb49bddb989c1e3574949dc056b7e2f8cd32066
이경민(Kyung Min Lee) <114645806+kminsmin@users.noreply.github.com>
2025-08-04 11:49:05 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int dp[1001] = { 0,1,2 };
+
+int main()
+{
+    cin >> n;
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = (dp[i - 1] + dp[i - 2])%10007;
+    }
+    cout << dp[n] << endl;
+    return 0;
+}
+
